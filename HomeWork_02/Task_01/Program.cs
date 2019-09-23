d4ffe18c347b67318a78fb5462c455ecf530efd2
Alex <potetenko.alexey@gmail.com>
2019-09-23 18:43:57 +0300

    Task_01 "Casting" done

---
+++ HomeWork_02/Task_01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //explicit casting
+
+            int a = 255;
+            byte b;
+            b = (byte)a;
+
+            long l = 123456789L;
+            int i;
+            i = (int)l;
+
+            double d = 3.14F;
+            float f;
+            f = (float)d;
+
+            Console.WriteLine("Expliocit casting: " + "\n" + i + "\n" + b + "\n" + f);
+            Console.ReadKey();
+
+            //implicit casting
+
+            short s = 256;
+            long k = 0;
+            k = s;
+
+            byte u = 255;
+            int z = 0;
+            z = u;
+
+
+
+
+        }
+    }
+}
