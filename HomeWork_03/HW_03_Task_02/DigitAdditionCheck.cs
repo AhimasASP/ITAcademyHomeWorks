c888097f914a1721e3ea7edc743dea27fbef5f1c
Alex <potetenko.alexey@gmail.com>
2019-09-26 18:22:33 +0300

    HW_03_Task_02 started

---
+++ HomeWork_03/HW_03_Task_02/DigitAdditionCheck.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HW_03_Task_01;
+
+namespace HW_03_Task_02
+{
+    class DigitAdditionCheck : DigitAddition
+    {
+    }
+}
---
+++ HomeWork_03/Task_01/DigitAddition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_03_Task_01
+{
+    public class DigitAddition
+    {
+        public int CheckForDigit()
+        {
+            int value = 0;
+            bool isDigit = false;
+            while (isDigit == false)
+            {
+                try
+                {
+                    value = Convert.ToInt32(Console.ReadLine());
+                    isDigit = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error! Please enter digit format value!");
+                }
+            }
+            return value;
+        }
+        public int SimpleAdditionTwoValues()
+
+        {
+            int intValue;
+            int result;
+            Console.WriteLine("\nPlease enter the first value");
+            Console.Write((intValue = CheckForDigit()) + " - first value;\nPlease enter the second value\n");
+            result = intValue;
+            Console.WriteLine((intValue = CheckForDigit()) + " - second value;");
+            return result += intValue;
+        }
+    }
+}
