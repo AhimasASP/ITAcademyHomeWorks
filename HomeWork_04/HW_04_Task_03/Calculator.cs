7422fc3f9ecab3360f8b141c82d17d00b05bba44
Alex <potetenko.alexey@gmail.com>
2019-10-02 18:52:42 +0300

    All tasks in HW_04 done

---
+++ HomeWork_04/HW_04_Task_03/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace HW_04_Task_03
+{
+    public class Calculator
+    {
+        public void Calculate(int sum, byte periodInMonth)
+        {
+            DateTime date = new DateTime(2019, 9, 1);
+            decimal currentSum = sum;
+            decimal increase;
+            decimal capitalization;
+            string currency = " BYN";
+            
+            for (int i = 0; i < periodInMonth; i++)
+            {
+                date = date.AddMonths(1);
+                Console.WriteLine("\nOn the date of " + date.ToString("MMMM yyyy", CultureInfo.CurrentCulture) + 
+                                  "\n\n\tIncrease of contribution is:\t" + 
+                                  (increase = (capitalization = currentSum * 2 / 100) + currentSum - sum).ToString("0.00") + currency + 
+                                  "\n\tThe capitalization is:\t" + (capitalization).ToString("0.00") + currency +
+                                  "\n\tCurrent contribution sum is:\t" + (currentSum += (currentSum * 2 / 100)).ToString("0.00") + currency);
+            }
+        }
+    }
+}
---
+++ HomeWork_04/HW_04_Task_03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW_04_Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("\tHello! Welcome to IT-Bank Calculator.\nResults of calculation:\n");
+            calculator.Calculate(1000, 12);
+            Console.WriteLine(new string('-', 100) + "\n\tTHANKS FOR CHOOSING US");
+        }
+    }
+}
