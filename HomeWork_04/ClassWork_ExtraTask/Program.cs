d4e7bd1eacc079773d6ae65867c91411f9665a82
Alex <potetenko.alexey@gmail.com>
2019-10-02 23:09:49 +0300

    ExtraTask done

---
+++ HomeWork_04/ClassWork_ExtraTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassWork_ExtraTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please enter random word");
+            string word = Console.ReadLine();
+            Console.WriteLine("\n\tThe count of letters 'a' in string " + word + " are: " + (word.Split('a').Length -1));
+        }
+    }
+}
