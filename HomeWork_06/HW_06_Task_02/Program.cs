fd343a7860e2eec6a496b4b4510c013cc6fe126b
Alex <potetenko.alexey@gmail.com>
2019-10-07 19:03:53 +0300

    HW_06_Task_02 done

--- HomeWork_06/HW_06_Task_02/Program.cs
+++ HomeWork_06/HW_06_Task_02/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Net;
 
 namespace HW_06_Task_02
 {
@@ -10,9 +9,10 @@ namespace HW_06_Task_02
         {   
             string srcString = File.ReadAllText(Environment.CurrentDirectory.Replace(@"HW_06_Task_02\bin\Debug\netcoreapp3.0", "") + @"\panagramm.txt");
             StringModifier modifier = new StringModifier(srcString);
-            Console.WriteLine("There is source string: " + srcString + "\nThere source string without longest word: " + 
-                              modifier.longestWordRemoving() + "\nThere is string with replacement of longest and shortest words: " + modifier.LongestShortestPlaceChanging());
-
+            Console.WriteLine("There is source string: " + srcString + "\nThere source string without longest word: " + modifier.LongestWordRemoving() + 
+                              "\nThere is string with replacement of longest and shortest words: " + modifier.LongestShortestPlaceChanging() + 
+                              "\nLetters counter is: " + modifier.LettersCounter() + "\nPunctuation counter is: " + modifier.PunctuationCounter() + 
+                              "\nSource string to sorted array is: " + modifier.ShowArrayAsString(modifier.StringToSortedArray()));
         }
     }
 }
--- HomeWork_06/HW_06_Task_02/StringModifier.cs
+++ HomeWork_06/HW_06_Task_02/StringModifier.cs
@@ -40,7 +40,7 @@ namespace HW_06_Task_02
             return regex.Replace(str, "");
         }
 
-        public string longestWordRemoving()
+        public string LongestWordRemoving()
         {
             return str.Replace(longestWord, "");
         }
@@ -65,11 +65,44 @@ namespace HW_06_Task_02
             return builder.ToString().Substring(0, builder.ToString().Length - 1);
         }
 
-        public int  LettersCounter()
+        public int LettersCounter()
         {
-            int counter = 0;
-            str.ToCharArray();
-            return counter;
+            return PunctuationFilter(str).Length;
+        }
+
+        public int PunctuationCounter()
+        {
+            string tmpString = str.Replace(" ", "");
+            return (tmpString.Length - PunctuationFilter(tmpString).Length);
+        }
+
+        public string ShowArrayAsString(string[] arrayToString)
+        {
+            builder.Clear();
+            builder.Append('[');
+            foreach (string currentString in arrayToString)
+            {
+                builder.Append(currentString);
+                builder.Append(' ');
+            }
+            return (builder.ToString().Substring(0, builder.ToString().Length - 1) + ']').ToLower();
+        }
+
+        public string[] StringToSortedArray()
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (PunctuationFilter(array[j]).Length < PunctuationFilter(array[j + 1]).Length)
+                    {
+                        string tmpString = PunctuationFilter(array[j + 1]);
+                        array[j + 1] = PunctuationFilter(array[j]);
+                        array[j] = tmpString;
+                    }
+                }
+            }
+            return array;
         }
     }
 }
