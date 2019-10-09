316a6f93473019fe8d80ea012eb6184b582ef1fb
Alex <potetenko.alexey@gmail.com>
2019-10-09 19:03:54 +0300

    All tasks done

---
+++ HomeWork_06/HW_06_ExtraTask/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW_06_ExtraTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am " + 18 + ".");
+            Console.WriteLine("I am " + 18.ToString() + ".");
+        }
+    }
+}
