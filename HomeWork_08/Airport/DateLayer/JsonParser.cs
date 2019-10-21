681a4ccfd5ec154b9fb9e8bac3bc18a0bf21a191
Alex <potetenko.alexey@gmail.com>
2019-10-21 22:00:40 +0300

    HW_08 started. JSON files added

---
+++ HomeWork_08/Airport/DateLayer/JsonParser.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace Airport
+{
+    public class JsonParser
+    {
+        private Person[] person = JsonSerializer.Deserialize<Person[]>(File.ReadAllText("Persons.json"));
+        private Flight[] flights = JsonSerializer.Deserialize<Flight[]>(File.ReadAllText("Flights.json"));
+    }
+}
